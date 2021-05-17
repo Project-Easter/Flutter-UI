ec9adf277761d4339c6239c0cda964272b9fdd19
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-05-17 19:37:58 +0200

    Refactor Utils/Api.dart

--- lib/Utils/Api.dart
+++ lib/Utils/Api.dart
@@ -1,65 +1,38 @@
+import 'dart:async';
 import 'package:http/http.dart';
 
 class Api {
   static const String BASE_ROUTE = "https://explr-api.herokuapp.com/api/v1";
 
-  static Future<Response> register(String email, String password) async {
+  static Future<Response> sendRequest(Future<Response> Function() request) async {
     var response;
 
     try {
-      response = await post(Uri.parse(BASE_ROUTE + "/auth/new-account"), body: {"email": email, "password": password});
+      response = await request();
     } catch (error) {
       print(error.toString());
     }
 
     return response;
+  }
+
+  static Future<Response> register(String email, String password) {
+    return sendRequest(() => post(Uri.parse(BASE_ROUTE + "/auth/new-account"), body: {"email": email, "password": password}));
   }
 
   static Future<Response> login(String email, String password) async {
-    var response;
-
-    try {
-      response = await post(Uri.parse(BASE_ROUTE + "/auth/email"), body: {"email": email, "password": password});
-    } catch (error) {
-      print(error.toString());
-    }
-
-    return response;
+    return sendRequest(() => post(Uri.parse(BASE_ROUTE + "/auth/email"), body: {"email": email, "password": password}));
   }
 
   static Future<Response> forgotPassword(String email) async {
-    var response;
-
-    try {
-      response = await post(Uri.parse(BASE_ROUTE + "/mail/password-reset"), body: {"email": email});
-    } catch (error) {
-      print(error.toString());
-    }
-
-    return response;
+    return sendRequest(() => post(Uri.parse(BASE_ROUTE + "/mail/password-reset"), body: {"email": email}));
   }
 
   static Future<Response> resetPassword(String email, String password, String code) async {
-    var response;
-
-    try {
-      response = await patch(Uri.parse(BASE_ROUTE + "/user/password"), body: {"email": email, "password": password, "code": code});
-    } catch (error) {
-      print(error.toString());
-    }
-
-    return response;
+    return sendRequest(() => patch(Uri.parse(BASE_ROUTE + "/user/password"), body: {"email": email, "password": password, "code": code}));
   }
 
   static Future<Response> confirmEmail(String email, String code) async {
-    var response;
-
-    try {
-      response = await post(Uri.parse(BASE_ROUTE + "/user/email"), body: {"email": email, "code": code});
-    } catch (error) {
-      print(error.toString());
-    }
-
-    return response;
+    return sendRequest(() => post(Uri.parse(BASE_ROUTE + "/user/email"), body: {"email": email, "code": code}));
   }
 }
