064d80895bc95c5da4bb01b09f3c39e97f62bbf0
Akeem Tobi <akeemtobi6@gmail.com>
2021-07-31 00:20:20 +0100

    Dark mode switching

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:books_app/Constants/routes.dart';
 import 'package:books_app/Services/auth.dart';
 import 'package:books_app/Utils/router.dart';
 import 'package:books_app/Utils/theme_notifier.dart';
-import 'package:books_app/Utils/values/theme_switch.dart';
 import 'package:books_app/models/books.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:books_app/common/themes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +18,7 @@ Future<void> main() async {
         MultiProvider(
           providers: <ChangeNotifierProvider<dynamic>>[
             ChangeNotifierProvider<ThemeNotifier>(
-              create: (_) => ThemeNotifier(darkTheme),
+              create: (_) => ThemeNotifier(lightTheme),
             ),
             ChangeNotifierProvider<Books>(
               create: (_) => Books(),
@@ -35,11 +35,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dynamic myAppUser = AuthService().currentUserFromFireBase;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Explr',
-      initialRoute: myAppUser == null ? Routes.INITIAL_PAGE : Routes.HOME,
-      onGenerateRoute: RouteGenerator.generateRoute,
-    );
+    return Consumer<ThemeNotifier>(
+        builder: (BuildContext context, ThemeNotifier theme, _) => MaterialApp(
+              theme: theme.getTheme(),
+              debugShowCheckedModeBanner: false,
+              title: 'Explr',
+              initialRoute:
+                  myAppUser == null ? Routes.INITIAL_PAGE : Routes.HOME,
+              onGenerateRoute: RouteGenerator.generateRoute,
+            ));
   }
 }
