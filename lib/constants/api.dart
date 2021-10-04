b099440c519b802dd0934bcd14c97a1b622c4099
Mohit_007 <mohitmadhav88@gmail.com>
2021-10-04 14:58:31 +0530

    deleted node api

--- lib/constants/api.dart
+++
@@ -1,3 +0,0 @@
-const String BASE_ROUTE = 'https://explr-api.herokuapp.com/api/v1';
-const String PROFILE_PIC_ROUTE =
-    'https://res.cloudinary.com/primaxx-cloud/image/upload/v1609774441/avatars/';
