27b82d246015fcba54bc09df1f44cb5c58fb75c7
Mohit_007 <mohitmadhav88@gmail.com>
2021-10-19 10:17:27 +0530

    appbar font change

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -5,24 +5,23 @@ import 'package:google_fonts/google_fonts.dart';
 // ignore: non_constant_identifier_names
 AppBar MyAppBar(BuildContext buildContext) {
   return AppBar(
-    // shadowColor: Colors.black,
+   
     bottomOpacity: 0.5,
     titleSpacing: 5,
     elevation: 0.0,
-    // backgroundColor: Theme.of(buildContext).backgroundColor,
+    
     leading: Padding(
       padding: const EdgeInsets.all(12.0),
       child: Image.asset('assets/images/ExplrLogo(150x150).png'),
     ),
     title: Text(
       'Explr',
-      style: GoogleFonts.mali(color: Colors.black),
+      style: GoogleFonts.lato(),
     ),
     actions: <Widget>[
       IconButton(
-        icon: Icon(
+        icon: const Icon(
           Icons.settings,
-          // color: Colors.black,
           size: 20,
         ),
         onPressed: () {
