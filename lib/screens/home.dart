3c1e1c495e0ae958397293b4e42dcbd4ebc0a62c
Mohit_007 <mohitmadhav88@gmail.com>
2021-11-01 00:00:16 +0530

    lint

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -58,7 +58,7 @@ class _HomeState extends State<Home> {
         ),
         StreamProvider<List<Book>>.value(
           value: _databaseService.booksData,
-          initialData: <Book>[],
+          initialData: const <Book>[],
         ),
       ],
       child: Scaffold(
